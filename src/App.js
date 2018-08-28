2577db9e180370c3addd32f809846a44edb1f592
Yuri Apollov <apollovy@gmail.com>
2018-08-28 12:45:53 +0300

    Install react. Write initial app implementation.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import './App.css';
+import Form from "./Form";
+
+class App extends Component {
+    render() {
+        return (
+            <Form plantuml_url="https://plantuml.com/plantuml"/>
+        );
+    }
+}
+
+export default App;
---
+++ src/Form.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+import plantumlEncoder from "plantuml-encoder";
+
+class Form extends Component {
+    plantuml_url: string;
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: '',
+        };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    render() {
+        return (
+            <form method="GET" onSubmit={this.handleSubmit}>
+                <textarea onChange={this.handleChange}/>
+                <input type="submit"/>
+            </form>
+        )
+    }
+
+    handleSubmit(event: Event) {
+        event.preventDefault();
+
+        let encoded = plantumlEncoder.encode(this.state.text);
+        console.log(encoded);
+        // let url = this.props.plantuml_url + '/png/' + encoded;
+
+    }
+
+    handleChange(event: Event) {
+        this.setState({text: event.target.value})
+    }
+}
+
+export default Form;
