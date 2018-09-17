3fe59294d4812f9dd1e3537f1bcb5557485b8979
Yuri Apollov <apollovy@gmail.com>
2018-09-17 21:04:59 +0300

    Make test step happy about localStorage.

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// noinspection ES6UnusedImports
+import LocalStorageMock from './LocalStorageMock'
 import App from './App';
 
 it('renders without crashing', () => {
